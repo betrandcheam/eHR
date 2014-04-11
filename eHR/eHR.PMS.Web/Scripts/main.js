17f66170efe2136fdebb4104b72449df986420a5
kyoliumiao <liumiao109@gmail.com>
2014-04-11 18:42:37 +0800

    ie8 array indexof

--- eHR/eHR.PMS.Web/Scripts/main.js
+++ eHR/eHR.PMS.Web/Scripts/main.js
@@ -1,6 +1,29 @@
 ﻿var specialCharArray = [":", ";", "/", "?", "@", "&", "=", "+", "$", "#"];
 var specialCharArrayString = ":;/?@&=+$#";
 var ErrorMessgae = "You are not allowed to enter more than 2 special characters (" + specialCharArrayString + ") in a continuous sequence.";
+if (!Array.prototype.indexOf)
+{
+  Array.prototype.indexOf = function(elt /*, from*/)
+  {
+    var len = this.length >>> 0;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++)
+    {
+      if (from in this &&
+          this[from] === elt)
+        return from;
+    }
+    return -1;
+  };
+}
+
 function IsSpecialChar(str) {
     for (var k=0;k<specialCharArray.length;k++) {
         var index=str.indexOf(specialCharArray[k])
